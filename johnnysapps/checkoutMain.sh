fcc872da608af1f66b1b3804aa8321b6cfc1ab6b
Ionica Bizau <bizauionica@yahoo.com>
2013-08-19 09:02:11 +0300

    add checkout main

---
+++ johnnysapps/checkoutMain.sh
@@ -0,0 +1,9 @@
+#########################################################
+# Checkout main                                         #
+#########################################################
+# Follow me on Github :: https://github.com/IonicaBizau #
+#########################################################
+
+echo "> Checkout main."
+git checkout master
+git checkout main.js
