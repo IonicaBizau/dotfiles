6652541e69f4db74d59d983717686306a0513671
Ionica Bizau <bizauionica@yahoo.com>
2013-08-19 09:02:43 +0300

    add Johnny's apps

---
+++ johnnysapps/Master.sh
@@ -0,0 +1,8 @@
+#########################################################
+# Checkout master                                       #
+#########################################################
+# Follow me on Github :: https://github.com/IonicaBizau #
+#########################################################
+
+echo "> Checkout master."
+git checkout master
---
+++ johnnysapps/moveDev.sh
@@ -0,0 +1,10 @@
+#########################################################
+# Move dev git tag                                      #
+#########################################################
+# Follow me on Github :: https://github.com/IonicaBizau #
+#########################################################
+
+echo "> Move dev."
+git tag  -d dev
+git tag     dev
+git push --tags
