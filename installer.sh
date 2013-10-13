880b69b5cb81265edf320c6f90ba09676e52c3ff
Ionică Bizău <bizauionica@yahoo.com>
2013-10-13 15:16:21 +0300

    Added the installation scripts.

--- installer.sh
+++ installer.sh
@@ -1,1 +1,13 @@
 echo "~ ~ ~ Installing config files ~ ~ ~"
+
+echo "Clonning the repository..."
+git clone https://github.com/IonicaBizau/configs
+
+echo "Entering in the downloaded folder."
+cd configs
+
+echo "Copying Johnny's apps"
+sudo cp -r johnnysapps /
+
+echo "Copying VIM files."
+cp .vim* ~/
