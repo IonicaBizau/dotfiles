80d4a4ec0a81933f5055b67325331760b30a5672
Ionică Bizău <bizauionica@yahoo.com>
2013-10-17 13:22:35 +0300

    Qt installer

---
+++ utils/qt.sh
@@ -0,0 +1,4 @@
+# Install Qt
+wget http://download.qt-project.org/official_releases/online_installers/1.4/qt-linux-opensource-1.4.0-2-x86-online.run -O qt-installer.run
+chmod +x qt-installer.run
+./qt-installer.run
