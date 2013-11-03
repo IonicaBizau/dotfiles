44a36a480f504d346457c48a0d2d8f689908d5c6
Ionică Bizău <bizauionica@yahoo.com>
2013-11-03 13:07:11 +0200

    Create get-vim.sh

---
+++ utils/get-vim.sh
@@ -0,0 +1,31 @@
+# Compile VIM from source
+# Thanks: https://github.com/Valloric/YouCompleteMe/wiki/Building-Vim-from-source
+
+sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial
+
+sudo apt-get remove vim vim-runtime gvim
+sudo apt-get remove vim-tiny vim-common vim-gui-common
+
+cd ~
+hg clone https://code.google.com/p/vim/
+cd vim
+./configure --with-features=huge \
+            --enable-rubyinterp \
+            --enable-pythoninterp \
+            --with-python-config-dir=/usr/lib/python2.7-config \
+            --enable-perlinterp \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+make VIMRUNTIMEDIR=/usr/share/vim/vim74
+sudo make install
+
+sudo apt-get install checkinstall
+cd vim
+sudo checkinstall
+
+# install git
+sudo apt-get install git
+
+# install vundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
