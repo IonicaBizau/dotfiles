b1dc625b71360823d27227e99a39d3cd80dd25fd
Ionică Bizău <bizauionica@yahoo.com>
2013-11-06 10:39:39 +0200

    get libs

--- utils/qt.sh
+++ utils/qt.sh
@@ -1,3 +1,7 @@
+# Get the libs, first
+sudo apt-get install mesa-common-dev
+sudo apt-get install libgl1-mesa-dev libglu1-mesa-dev
+
 # Install Qt
 wget http://download.qt-project.org/official_releases/online_installers/1.4/qt-linux-opensource-1.4.0-2-x86-online.run -O qt-installer.run
 chmod +x qt-installer.run
