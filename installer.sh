56e7f1ca16c89e6f9e62e8cc4ef9e556ece4a64c
Ionică Bizău <bizauionica@yahoo.com>
2013-12-08 09:44:41 +0200

    Install vundle.

--- installer.sh
+++ installer.sh
@@ -1,17 +1,20 @@
 echo "~ ~ ~ Installing config files ~ ~ ~"
 
-echo "Clonning the repository..."
+echo "Clonning the repository ..."
 git clone https://github.com/IonicaBizau/configs
 
 echo "Entering in the downloaded folder."
 cd configs
 
-echo "Copying Johnny's apps"
+echo "Copying Johnny's apps ..."
 sudo cp -r johnnysapps /
 
-echo "Copying VIM files."
+echo "Copying VIM files ..."
 cp -r vim-files/.vim* ~/
 
-echo "Finished. Clean up..."
+echo "Installing Vundle ..."
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+echo "Finished. Clean up ..."
 cd ..
 rm -rf configs
