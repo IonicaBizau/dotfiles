bccc3c6296e7db94974fd72fe8a02ed630dda582
Ionică Bizău <bizauionica@yahoo.com>
2014-01-02 10:19:07 +0200

    Create install-node-and-mongo.sh

---
+++ utils/install-node-and-mongo.sh
@@ -0,0 +1,12 @@
+# install node
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+ 
+# install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+sudo apt-get update
+sudo apt-get install mongodb-10gen
