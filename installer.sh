c226b06c97e6e79ed7cc24795e008c80da533c25
Ionică Bizău <bizauionica@yahoo.com>
2014-01-26 21:41:50 +0200

    install vim

--- installer.sh
+++ installer.sh
@@ -9,6 +9,9 @@ cd configs
 echo "Copying Johnny's apps ..."
 sudo cp -r johnnysapps /
 
+echo "Installing VIM ..."
+sudo apt-get install -y vim
+
 echo "Copying VIM files ..."
 cp -r vim-files/.vim* ~/
 
