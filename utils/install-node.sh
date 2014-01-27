2a4691b2d1e0823bbde0f1bda9ece48fcf85c311
Ionică Bizău <bizauionica@yahoo.com>
2014-01-27 20:59:23 +0200

    Use -y argument

--- utils/install-node.sh
+++ utils/install-node.sh
@@ -1,4 +1,4 @@
-sudo apt-get install python-software-properties python g++ make
-sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js -y
 sudo apt-get update
-sudo apt-get install nodejs
+sudo apt-get install nodejs -y
