c03f23d5f58a1cd23c2e1a406fcd85570ea60ad0
Ionică Bizău <bizauionica@yahoo.com>
2014-02-08 18:39:17 +0200

    Added -y argument

--- utils/get-vim.sh
+++ utils/get-vim.sh
@@ -3,10 +3,10 @@
 
 sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
 libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial
+libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial -y
 
-sudo apt-get remove vim vim-runtime gvim
-sudo apt-get remove vim-tiny vim-common vim-gui-common
+sudo apt-get remove vim vim-runtime gvim -y
+sudo apt-get remove vim-tiny vim-common vim-gui-common -y
 rm -rf .vim
 
 cd ~
