ac8866d88084eac458ca377aa2e9dfe1b80003f3
Ionică Bizău <bizauionica@yahoo.com>
2014-03-08 19:27:56 +0200

    Added the script that creates a gitignore file in the current directory.

---
+++ johnnysapps/ignore.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#########################################################
+# Create a gitignore file in the current directory      #
+#########################################################
+# Follow me on Github :: https://github.com/IonicaBizau #
+#########################################################
+
+echo "> Creating .gitignore file."
+cp /johnnysapps/gitignore $PWD/.gitignore
