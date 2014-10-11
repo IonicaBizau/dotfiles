342a1908a757d6ff7ebc682ae135537f8a1a5743
Ionică Bizău <bizauionica@yahoo.com>
2014-10-11 21:58:27 +0300

    Removed comments

--- installer.sh
+++ installer.sh
@@ -9,30 +9,30 @@ echo "                                                                       "
 echo "            This repository contains personal dotfiles of Ionică Bizău."
 echo "                              - - - - - - - - - -                      "
 
-#echo "Installing git ..."
-#sudo apt-get install git -y
-#
-#echo "Installing curl ..."
-#sudo apt-get install curl -y
-#
-#echo "Clonning the repository ..."
-#git clone https://github.com/IonicaBizau/dotfiles
-#
-#echo "Entering in the downloaded folder."
-#cd dotfiles
-#
-#echo "Copying Johnny's apps ..."
-#sudo cp -r johnnysapps /
-#
-#echo "Installing VIM ..."
-#sudo apt-get install -y vim
-#
-#echo "Copying VIM files ..."
-#cp -r vim-files/.vim* ~/
-#
-#echo "Installing NeoBundle ..."
-#curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-#
-#echo "Finished. Clean up ..."
-#cd ..
-#rm -rf dotfiles
+echo "Installing git ..."
+sudo apt-get install git -y
+
+echo "Installing curl ..."
+sudo apt-get install curl -y
+
+echo "Clonning the repository ..."
+git clone https://github.com/IonicaBizau/dotfiles
+
+echo "Entering in the downloaded folder."
+cd dotfiles
+
+echo "Copying Johnny's apps ..."
+sudo cp -r johnnysapps /
+
+echo "Installing VIM ..."
+sudo apt-get install -y vim
+
+echo "Copying VIM files ..."
+cp -r vim-files/.vim* ~/
+
+echo "Installing NeoBundle ..."
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+
+echo "Finished. Clean up ..."
+cd ..
+rm -rf dotfiles
