efe9067e119bb92595bf75f864b3fc45f2abf874
Ionică Bizău <bizauionica@yahoo.com>
2014-12-20 19:51:15 +0200

    Install mongodb-org instead.

--- apps/node-and-mongo.sh
+++ apps/node-and-mongo.sh
@@ -7,6 +7,6 @@ sudo apt-get install nodejs -y
  
 # install mongodb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
-sudo apt-get install mongodb-10gen  -y
+sudo apt-get install -y mongodb-org
