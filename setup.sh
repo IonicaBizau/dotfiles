7ef0daf5b8b6e39a0632a74a429eae1bee43a326
Ionică Bizău <bizauionica@gmail.com>
2019-11-24 19:50:05 -0300

    cleanup and setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,17 @@
-ln -s $HOME/Documents/dotfiles/blah-templates $HOME/.blah-templates
-ln -s $HOME/Documents/dotfiles/blah-templates $HOME/.blah-templates
+ln -s $HOME/Docs/dotfiles/blah-templates $HOME/.blah-templates
+
+linkUser () {
+	rm $HOME/$1
+	echo "Linking ~/Docs/dotfiles/$1 to $HOME/$1"
+	ln -s "$HOME/Docs/dotfiles/user/$1" "$HOME/$1"
+}
+
+linkUser .vimrc
+linkUser .babelrc
+linkUser .bashrc
+linkUser .bible-config.json
+linkUser .gitconfig
+linkUser .jshintrc
+linkUser .npmrc
+linkUser .packy.js
+linkUser .tmux.conf
