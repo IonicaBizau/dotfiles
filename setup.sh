7c59d1bb2a48689c7c5fc519718d1e013ba7513f
Ionică Bizău <bizauionica@gmail.com>
2020-08-04 15:38:00 +0300

    Remove token

--- setup.sh
+++ setup.sh
@@ -9,12 +9,18 @@ linkUser () {
     ln -s "$HOME/Docs/dotfiles/user/$1" "$HOME/$1"
 }
 
+cpUser () {
+    rm $HOME/$1
+    echo "Copying ~/Docs/dotfiles/$1 to $HOME/$1"
+    cp "$HOME/Docs/dotfiles/user/$1" "$HOME/$1"
+}
+
 linkUser .vimrc
 linkUser .babelrc
 linkUser .bashrc
 linkUser .bible-config.json
 linkUser .gitconfig
 linkUser .jshintrc
-linkUser .npmrc
+cpUser .npmrc
 linkUser .packy.js
 linkUser .tmux.conf
