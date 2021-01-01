ca353aba830fef1e69efa3ab187121750016c667
Ionică Bizău <bizauionica@gmail.com>
2021-01-01 11:24:01 +0200

    small fixes

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT")
 
+rm -rf $HOME/.blah-templates
 ln -s $BASEDIR/blah-templates $HOME/.blah-templates
 
 linkUser () {
-    rm $HOME/$1
+    rm -rf $HOME/$1
     echo "Linking ~/Docs/dotfiles/$1 to $HOME/$1"
     ln -s "$HOME/Docs/dotfiles/user/$1" "$HOME/$1"
 }
 
 cpUser () {
-    rm $HOME/$1
+    rm -rf $HOME/$1
     echo "Copying ~/Docs/dotfiles/$1 to $HOME/$1"
     cp "$HOME/Docs/dotfiles/user/$1" "$HOME/$1"
 }
@@ -24,3 +25,4 @@ linkUser .jshintrc
 cpUser .npmrc
 linkUser .packy.js
 linkUser .tmux.conf
+echo "Done."
--- user/.bashrc
+++ user/.bashrc
@@ -37,7 +37,7 @@ fi
 
 # set a fancy prompt (non-color, unless we know we "want" color)
 case "$TERM" in
-    xterm-color) color_prompt=yes;;
+    xterm-color|*-256color) color_prompt=yes;;
 esac
 
 # uncomment for a colored prompt, if the terminal has the capability; turned
@@ -84,6 +84,8 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep='egrep --color=auto'
 fi
 
+# colored GCC warnings and errors
+#export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 # some more ls aliases
 alias ll='ls -alF'
 alias la='ls -A'
